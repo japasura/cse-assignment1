a37f2373928b0c9c764a8c687dc818662a0939bf
Samarth <samarthr1@outlook.com>
2022-09-28 08:36:30 +0530

    FEATURE: impl queue.c

--- queue.c
+++ queue.c
@@ -1,46 +1,54 @@
 #include "queue.h"
+#include "dll.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-queue_t* create_queue()   // return a newly created empty queue
-{
-	// DO NOT MODIFY!!!
-	queue_t* Q = (queue_t*) malloc(sizeof(queue_t));
-	Q->list = create_list();
-	Q->front = Q->list->head;
-	Q->rear = Q->list->tail;
-	Q->size = Q->list->size;
-	return Q;
+
+/* return a newly created empty queue */
+queue_t *create_queue ( ) {
+    // DO NOT MODIFY!!!
+    queue_t *Q = ( queue_t * ) malloc( sizeof( queue_t ));
+    Q->list = create_list();
+    Q->front = Q->list->head;
+    Q->rear = Q->list->tail;
+    Q->size = Q->list->size;
+    return Q;
 }
 
-void enqueue(queue_t* q, int data) // TODO: insert data at the end of a queue
-{
+/*  insert data at the end of a queue */
+void enqueue ( queue_t *q, int data ) {
+    insert_back( q->list, data );
+    q->rear = q->list->tail;
+    q->size++;
 }
 
-int dequeue(queue_t* q) 	// TODO: return the data at the front of a queue and remove it. Return -1 if queue is empty
-{
-
+/* return the data at the front of a queue and remove it. Return -1 if queue is empty */
+int dequeue ( queue_t *q ) {
+    int rv = q->front->data;
+    delete_front( q->list );
+    return rv;
 }
 
-int front(queue_t* q) // TODO: return the data at the front of a queue. Return -1 if queue is empty
-{
+/* return the data at the front of a queue. Return -1 if queue is empty */
+int front ( queue_t *q ) {
+    return q->front->data;
 }
 
-int empty(queue_t* q) // return if the queue is empty
+int empty ( queue_t *q ) // return if the queue is empty
 {
-	// DO NOT MODIFY!!!
-	return is_empty(q->list);
+    // DO NOT MODIFY!!!
+    return is_empty( q->list );
 }
 
-int queue_size(queue_t* q) // returns the number of elements in the queue
+int queue_size ( queue_t *q ) // returns the number of elements in the queue
 {
-	// DO NOT MODIFY!!!
-	return q->size;
+    // DO NOT MODIFY!!!
+    return q->size;
 }
 
-void delete_queue(queue_t* q) // empty the contents of the queue
+void delete_queue ( queue_t *q ) // empty the contents of the queue
 {
-	// DO NOT MODIFY!!!
-	delete_list(q->list);
-	free(q);
+    // DO NOT MODIFY!!!
+    delete_list( q->list );
+    free( q );
 }
